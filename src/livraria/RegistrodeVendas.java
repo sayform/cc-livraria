bcca72de9199cfe74471a5916e968a9980b67071
Sammy F <sayform@gmail.com>
2017-01-04 21:25:43 -0200

    alteração na classe RegistroDeVenda e criação da CarrinhoDeCompra

--- src/livraria/RegistrodeVendas.java
+++ src/livraria/RegistrodeVendas.java
@@ -8,9 +8,17 @@ public class RegistrodeVendas {
 		
 		LivroFisico fisico = new LivroFisico(autor);
 		fisico.setTitulo("Test-Driven Development");
+		fisico.setValor(59.90);
 		
 		Ebook ebook = new Ebook(autor);
 		ebook.setTitulo("Test-Driven Development");
+		ebook.setValor(29.90);
+		
+		CarrinhoDeCompras cc = new CarrinhoDeCompras();
+		cc.Adiciona(fisico);
+		cc.Adiciona(ebook);
+		
+		System.out.println("Valor Total " + cc.getTotal());
 	}
 
 }
